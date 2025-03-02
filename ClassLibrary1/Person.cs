592e60718362e8ad43cc411343d5df5ec4afccac
NatUsik6 <tashausik6@gmail.com>
2025-03-02 20:00:43 +0300

    feat: crete first app

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    public class Person
+    {
+        string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+        public void Print() => Console.WriteLine($"Name: {name}");
+    }
+}
