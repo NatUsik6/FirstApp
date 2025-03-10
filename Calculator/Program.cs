fb88eb3cab8b5b566c843a0e1542e66b38cb8ef0
NatUsik6 <tashausik6@gmail.com>
2025-03-10 10:29:15 +0300

    create calculator and seasons

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, I'm Calculator! Enter first number, operation and second number");
+int numberOne = int.Parse(Console.ReadLine());
+string operation = Console.ReadLine();
+int numberTwo = int.Parse(Console.ReadLine());
+
+switch (operation)
+{
+    case "+":
+        Console.WriteLine(numberOne + numberTwo);
+        break;
+    case "-":
+        Console.WriteLine(numberOne - numberTwo);
+        break;
+    case "*":
+        Console.WriteLine(numberOne * numberTwo);
+        break;
+    case "/":
+        Console.WriteLine(numberOne / numberTwo);
+        break;
+}
---
+++ Season/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary1;
+
+Console.WriteLine("Input month: ");
+int month = int.Parse(Console.ReadLine());
+
+if (month <= 2 || month == 12) 
+{
+    Console.WriteLine("Winter");
+}
+else if (month <= 5)
+{
+    Console.WriteLine("Spring");
+}
+else if (month <= 8)
+{
+    Console.WriteLine("Summer");
+}
+else if (month <= 11)
+{
+    Console.WriteLine("Fall");
+}
+else
+{
+    Console.WriteLine("Error");
+}
